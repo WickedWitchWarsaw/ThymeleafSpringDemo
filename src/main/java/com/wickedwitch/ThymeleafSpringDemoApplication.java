a0f4e18271de7039e2a64884f2cc470f55e3b2da
WickedWitchWarsaw <zuzka.pacak@gmail.com>
2017-01-09 16:20:46 +0100

    Initial commit - start of project

---
+++ src/main/java/com/wickedwitch/ThymeleafSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.wickedwitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSpringDemoApplication.class, args);
+	}
+}
