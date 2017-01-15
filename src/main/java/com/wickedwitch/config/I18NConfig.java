eb36384ed46835275569ac796d3c89c36fdd5666
WickedWitchWarsaw <zuzka.pacak@gmail.com>
2017-01-15 10:19:54 +0100

    i18n support added + language prpoerty files

---
+++ src/main/java/com/wickedwitch/config/I18NConfig.java
@@ -0,0 +1,46 @@
+package com.wickedwitch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * Created by ZuZ on 2017-01-14.
+ */
+@Configuration
+public class I18NConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    ReloadableResourceBundleMessageSource messageSource(){
+        ReloadableResourceBundleMessageSource bundleMessageSource = new ReloadableResourceBundleMessageSource();
+        bundleMessageSource.setBasename("classpath:i18n/messages");
+        bundleMessageSource.setCacheSeconds(1800);
+        return bundleMessageSource;
+    }
+
+    @Bean
+    LocaleResolver localeResolver(){
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
